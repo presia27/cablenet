ed81ddbcea6d1ce81d6fd516abcc907bb39bb51a
presia27 <pre.sia977@outlook.com>
2026-09-11 10:28:27 -0700

    Remove redundant array of metadata

--- main.go
+++ main.go
@@ -9,25 +9,34 @@ import (
 
 type StreamState struct {
 	metadata			utilities.StreamList
-	transcoder		transcode.Feed
+	transcoder		*transcode.Feed
 }
 
 var terminateTimeoutMil time.Duration = time.Duration(2000) * time.Millisecond
-var streamlist []utilities.StreamList		// list of feeds and their metadata
 var ffparams utilities.FFoptions				// default ffmpeg options
 
 var streamdata []StreamState						// metadata and reference to the process transcoding an active feed
 
 func main() {
 	// load feeds
-	utilities.LoadJsonFile("feeds.json", &streamlist)
-	streamInfo := utilities.GetStreamListInfo(streamlist)
-	fmt.Println("\nStream Metadata: \n", streamInfo)
+	var streamListParsed []utilities.StreamList
+	utilities.LoadJsonFile("feeds.json", &streamListParsed)
+	fmt.Println("\nStream Metadata: \n", utilities.GetStreamListInfo(streamListParsed))
+
+	// add feed to managed list of feeds
+	for _, f := range streamListParsed {
+		sd := StreamState{
+			metadata: f,
+			transcoder: nil,
+		}
+		streamdata = append(streamdata, sd)
+	}
+
 
 	// load ffmpeg default params
 	utilities.LoadJsonFile("ffmpegconf.json", &ffparams)
 
-	ffstring, err := utilities.BuildFFparams(streamlist[0], ffparams)
+	ffstring, err := utilities.BuildFFparams(streamdata[0].metadata, ffparams)
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -36,17 +45,7 @@ func main() {
 	if err != nil {
 		fmt.Println("Error: ", err)
 	}
-	var feedStreamState StreamState;
-	feedStreamState = StreamState{
-		metadata: streamlist[0],
-		transcoder: *feed,
-	}
-
-	// Add to list of managed streams
-	streamdata = append(streamdata, feedStreamState)
-	fmt.Print(streamdata)
-
-	//feed, _ := transcode.StartFeed(encodeString...)
+	streamdata[0].transcoder = feed
 
 	// input loop for testing
 	var cmd string;
@@ -58,7 +57,7 @@ func main() {
 
 		if cmd == "x" {
 			// stop feed
-			stopErr := transcode.StopFeed(&streamdata[0].transcoder, terminateTimeoutMil)
+			stopErr := transcode.StopFeed(streamdata[0].transcoder, terminateTimeoutMil)
 			fmt.Println("Stopped feed with code ", stopErr)
 
 			fmt.Println("Goodbye!")
